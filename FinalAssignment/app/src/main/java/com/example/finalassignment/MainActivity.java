e81d5c8e14198e3e9b9341bd2b180ef4cbe6b582
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-05-01 19:05:19 +0300

    added sqlite and created database for final courses

--- FinalAssignment/app/src/main/java/com/example/finalassignment/MainActivity.java
+++ FinalAssignment/app/src/main/java/com/example/finalassignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase database = this.openOrCreateDatabase("my_final_exams", MODE_PRIVATE, null);
+        database.execSQL("CREATE Table IF NOT EXISTS finals_courses (course_name VARCHAR, study_website VARCHAR)");
     }
 }
